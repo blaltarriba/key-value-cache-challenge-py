7a2fe126b7f5384ab52824b2f9e7b5c4d35540a6
Borja Lopez <borja.lopez@lana.xyz>
2021-10-09 22:58:23 +0200

    Add fixtures for cache service tests

--- cache/tests/services/test_cache.py
+++ cache/tests/services/test_cache.py
@@ -5,20 +5,27 @@ from cache.repositories.item_repository import ItemRepository
 
 class TestCache:
 
-    def test_fecth_item_return_item_when_exists(self) -> None:
-        item = Item(code='a_code', description='a_description')
-        item_repository = ItemRepository(items={ item.code: item })
-        cache = Cache(item_repository=item_repository)
+    @pytest.fixture
+    def an_item(self):
+        return Item(code='a_code', description='a_description')
 
-        result = cache.fetch(item.code)
+    @pytest.fixture
+    def an_item_repository(self, an_item):
+        return ItemRepository(items={ an_item.code: an_item })
 
-        assert result.code == item.code
-        assert result.description == item.description
+    @pytest.fixture
+    def a_cache_service(self, an_item_repository):
+        return Cache(item_repository=an_item_repository)
 
-    def test_fecth_item_return_none_when_does_not_exists(self) -> None:
-        item_repository = ItemRepository(items={})
-        cache = Cache(item_repository=item_repository)
+    def test_fecth_item_return_item_when_exists(self, a_cache_service: Cache, an_item: Item) -> None:
 
-        result = cache.fetch('fake')
+        result = a_cache_service.fetch(an_item.code)
+
+        assert result.code == an_item.code
+        assert result.description == an_item.description
+
+    def test_fecth_item_return_none_when_does_not_exists(self, a_cache_service: Cache) -> None:
+
+        result = a_cache_service.fetch('fake')
 
         assert result is None
